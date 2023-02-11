93b615c544ae5e43c3e307d506960334df683696
Guiritan <beejguiritan@gmail.com>
2023-02-11 17:32:34 +0800

    initial MyApplication Project

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,13 +21,9 @@ public class MainActivity extends AppCompatActivity {
         Log.v(TAG, "This is warn log") ;
         Log.e(TAG, "This is an error log");
 
-        Button button = (Button) findViewById(R.id.button);
+         Button button = findViewById(R.id.button);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {Log.i(TAG, "Button Clicked");
-            }
-        });
+          button.setOnClickListener(v -> Log.i(TAG, "Button Clicked"));
 
     }
 }
