75bf161b8c8010cb11a399246c5d6bc43626413b
Guiritan <beejguiritan@gmail.com>
2023-02-11 17:30:48 +0800

    initial MyApplication Project

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,10 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+
 import android.os.Bundle;
 import android.util.Log;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
