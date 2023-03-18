2917a1830dccee2a906d8b4d0126f458e3fbea22
Guiritan <guiritan112>
2023-03-18 17:41:25 +0800

    18/03/2023 Diogenes Guiritan

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setTitle("Ako si superman| ActionBar");
+
+        actionBar.setSubtitle("Design A custom action Bar");
+
+        actionBar.setIcon(R.mipmap.ic_launcher);
+
+        actionBar.setDisplayUseLogoEnabled(true);
+
+        actionBar.setDisplayShowHomeEnabled(true);
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+
+        switch (item.getItemId()) {
+            case R.id.search:
+                showAlertDialog();
+                break;
+            case R.id.refresh:
+                DialogFragment dialogFragment = new DialogFragment();
+                dialogFragment.show(getSupportFragmentManager(), "My Fragment");
+            case R.id.copy:
+                ;
+                Toast.makeText(this, "Copy Clicker", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+    private void showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Alert")
+                .setMessage("Danger you are failing");
+
+        builder.setPositiveButton("i know!", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog,int id){
+
+
+            }
+        });
+
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+
+    }
+}
